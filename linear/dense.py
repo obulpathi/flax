bcba62d06e35b83438661e48609e3f309da46bb1
Obulapathi N. Challa <obulpathi@gmail.com>
2022-09-11 11:31:42 +0530

    linear

---
+++ linear/dense.py
@@ -0,0 +1,23 @@
+import jax
+from jax import numpy as jnp, random, lax
+from flax.linen import initializers
+from typing import Callable
+from flax.linen import Module, compact
+
+
+class Dense(Module):
+  features: int
+  kernel_init: Callable = initializers.lecun_normal()
+  bias_init: Callable = initializers.zeros
+  use_bias: bool = True
+
+  @compact
+  def __call__(self, inputs):
+    kernel = self.param('kernel', self.kernel_init,
+                        (inputs.shape[-1], self.features))
+    y = lax.dot_general(inputs, kernel,
+                        (((inputs.ndim - 1,), (0,)), ((), ())),)
+    if self.use_bias:
+      bias = self.param('bias', self.bias_init, (self.features,))
+      y = y + bias
+    return y
---
+++ linear/regression.py
@@ -0,0 +1,34 @@
+import jax
+from jax import numpy as jnp, random, lax, jit
+from flax import linen as nn
+from dense import Dense
+
+
+X = jnp.ones((1, 10))
+Y = jnp.ones((5,))
+
+model = Dense(features=5)
+
+@jit
+def predict(params):
+  return model.apply({'params': params}, X)
+
+@jit
+def loss_fn(params):
+  return jnp.mean(jnp.abs(Y - predict(params)))
+
+@jit
+def init_params(rng):
+  mlp_variables = model.init({'params': rng}, X)
+  return mlp_variables['params']
+
+# Get initial parameters
+params = init_params(jax.random.PRNGKey(42))
+print("initial params", params)
+
+# Run SGD.
+for i in range(50):
+  loss, grad = jax.value_and_grad(loss_fn)(params)
+  print(i, "loss = ", loss, "Yhat = ", predict(params))
+  lr = 0.03
+  params = jax.tree_util.tree_map(lambda x, d: x - lr * d, params, grad)
